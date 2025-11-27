567412f5ea6b4f8e83f6c75292a73fa8cb35963f
Samar Balousha <2004samr@gmail.com>
2025-11-27 14:26:45 +0200

    Update Binary Search

--- DS_Chapter1/BinarySearch.java
+++ DS_Chapter1/BinarySearch.java
@@ -1,5 +1,41 @@
 package DS_Chapter1;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int array[] = {5, 12, 25, 45, 58, 95};
+        int target = 45;
+        int result = binarySearch(array, target);
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if target is present at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // If target is greater, ignore left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } 
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Target was not found in the array
+        return -1;
+    }
     
 }
